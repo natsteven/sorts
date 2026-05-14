c9fbf3035f7926587689438a39deddf0210e2209
Nathanael Steven <nathanaelsteven@gmail.com>
2026-05-14 13:11:01 -0600

    unsort

--- sorts.c
+++ sorts.c
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: 2026 Nathanael Steven
 // SPDX-License-Identifier: MIT
 
-static int SIZE = 15;
+#include <stdlib.h>
+
+static int SIZE = 16;
 
 void bubble(int *array) {
   int swapped;
@@ -43,34 +45,43 @@ void selection(int *array) {
   }
 }
 
-void merge_combine(int *p, int l, int m, int r) {
+void merge_combine(int *array, int l, int m, int r) {
   int n1 = m - l + 1;
   int n2 = r - m;
 
   int L[n1], R[n2];
 
-  for (int i = 0; i < n1; i++) L[i] = p[l + i];
-  for (int j = 0; j < n2; j++) R[j] = p[m + 1 + j];
+  for (int i = 0; i < n1; i++) L[i] = array[l + i];
+  for (int j = 0; j < n2; j++) R[j] = array[m + 1 + j];
 
   int i = 0, j = 0, k = l;
   while (i < n1 && j < n2) {
-    if (L[i] <= R[j]) { p[k] = L[i]; i++; }
-    else { p[k] = R[j]; j++; }
+    if (L[i] <= R[j]) { array[k] = L[i]; i++; }
+    else { array[k] = R[j]; j++; }
     k++;
   }
-  while (i < n1) { p[k] = L[i]; i++; k++; }
-  while (j < n2) { p[k] = R[j]; j++; k++; }
+  while (i < n1) { array[k] = L[i]; i++; k++; }
+  while (j < n2) { array[k] = R[j]; j++; k++; }
 }
 
-void merge_sort_recursive(int *p, int l, int r) {
+void merge_sort_recursive(int *array, int l, int r) {
   if (l < r) {
     int m = l + (r - l) / 2;
-    merge_sort_recursive(p, l, m);
-    merge_sort_recursive(p, m + 1, r);
-    merge_combine(p, l, m, r);
+    merge_sort_recursive(array, l, m);
+    merge_sort_recursive(array, m + 1, r);
+    merge_combine(array, l, m, r);
   }
 }
 
-void merge(int *p, int n) {
-  merge_sort_recursive(p, 0, n - 1);
+void merge(int *array, int n) {
+  merge_sort_recursive(array, 0, n - 1);
+}
+
+void unsort(int *array) {
+  for (int i=0; i < SIZE; i++) {
+    int j = rand() % SIZE;
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 }
